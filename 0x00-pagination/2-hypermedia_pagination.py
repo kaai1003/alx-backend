cff840ef95532f1aa5aa852014e1b87b0175e149
AIMAD KACEM <kacem.aimad@gmail.com>
2024-08-19 01:46:41 +0100

    2-hypermedia_pagination.py

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Hypermedia pagination Module"""
+import csv
+import math
+from typing import List, Tuple, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """return list dataset"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        range = self.index_range(page, page_size)
+        return self.dataset()[range[0]:range[1]]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """hypermedia function"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        size = len(self.dataset())
+        data = self.get_page(page, page_size)
+        ranges = self.index_range(page, page_size)
+        next_page = page + 1 if ranges[1] < size else None
+        prev_page = page - 1 if ranges[0] > 0 else None
+        total_pages = math.ceil(size / page_size)
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
+
+    @staticmethod
+    def index_range(page: int, page_size: int) -> Tuple:
+        """Simple helper function"""
+        end_index = page * page_size
+        start_index = end_index - page_size
+        return (start_index, end_index)

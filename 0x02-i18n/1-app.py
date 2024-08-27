598b1b0ba64682301617445519382796af8d9b45
AIMAD KACEM <kacem.aimad@gmail.com>
2024-08-27 19:47:12 +0100

    basic babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""basic babel setup"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """babel config class"""
+    LANGUAGES = ['en',
+                 'fr']
+    BABEL_DEFAULT_LOCAL = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """display “Hello world”"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

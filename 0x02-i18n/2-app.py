43e53c67aed41516db7ad9987f0e417821f88edc
AIMAD KACEM <kacem.aimad@gmail.com>
2024-08-27 20:20:23 +0100

    babel selector

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""basic babel setup"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """babel config class"""
+    LANGUAGES = ['en',
+                 'fr']
+    BABEL_DEFAULT_LOCAL = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """babel local selector func"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """display “Hello world”"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

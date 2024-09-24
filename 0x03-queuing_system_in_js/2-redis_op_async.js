f6c6e17a9543518c02e78bebe31a50fd18a5686a
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-24 17:16:50 +0100

    2-redis_op_async.js

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,27 @@
+import redis, { createClient } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, redis.print);
+};
+
+const getAsync = promisify(client.get).bind(client);
+
+const displaySchoolValue = async (schoolName) => {
+  const value = await getAsync(schoolName);
+  console.log(value);
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
